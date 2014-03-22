87b642c7cbd6172759a0023ed4012a058ade5292
Mohammad Abu-Garbeyyeh <mohammad7410@gmail.com>
2014-03-22 18:07:15 +0200

    Fix indents

--- src/com/mohammadag/unlockeffectbeforesecuritycode/UnlockEffect.java
+++ src/com/mohammadag/unlockeffectbeforesecuritycode/UnlockEffect.java
@@ -21,10 +21,10 @@ public class UnlockEffect implements IXposedHookLoadPackage {
 			//hookSettingsPackage(lpparam);
 			return;
 		}
-		
+
 		String packageName, javaPkg, keyguardUpdateMonitorName, securityModeEnumName, keyguardEffectViewName;
 		String backgroundName, foregroundName;
-		
+
 		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
 			packageName = "com.android.keyguard";
 			javaPkg = packageName;
@@ -34,15 +34,15 @@ public class UnlockEffect implements IXposedHookLoadPackage {
 			javaPkg = "com.android.internal.policy.impl.keyguard";
 			keyguardEffectViewName = javaPkg + "sec.KeyguardEffectView";
 		}
-		
+
 		keyguardUpdateMonitorName = javaPkg + ".KeyguardUpdateMonitor";
 		securityModeEnumName = javaPkg + ".KeyguardSecurityModel$SecurityMode";
 		backgroundName = keyguardEffectViewName + "$Background";
 		foregroundName = keyguardEffectViewName + "$Foreground";
-		
+
 		if (!lpparam.packageName.equals(packageName))
 			return;
-		
+
 		final Class<?> keyguardUpdateMonitor = findClass(keyguardUpdateMonitorName, lpparam.classLoader);
 		final Class<?> securityModeEnum = findClass(securityModeEnumName, lpparam.classLoader);
 		final Class<?> keyguardEffectViewClass = findClass(keyguardEffectViewName, lpparam.classLoader);
@@ -50,31 +50,31 @@ public class UnlockEffect implements IXposedHookLoadPackage {
 		final Class<?> backgroundEnum = findClass(backgroundName, lpparam.classLoader);
 		final Class<?> foregroundEnum = findClass(foregroundName, lpparam.classLoader);
 		final Class<?> KeyguardHostView = findClass(javaPkg + ".KeyguardHostView", lpparam.classLoader);
-		
+
 		XposedHelpers.findAndHookMethod(KeyguardHostView, "showPrimarySecurityScreen", boolean.class, new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) {
 				boolean turningOff = (Boolean) param.args[0];
 				Object mSecurityModel = getObjectField(param.thisObject, "mSecurityModel");	
-		        Object securityMode = XposedHelpers.callMethod(mSecurityModel, "getSecurityMode");
-		        Object mContext = getObjectField(param.thisObject, "mContext");
-		        
-		        Object keyguardUpdateMonitorInstance = XposedHelpers.callStaticMethod(keyguardUpdateMonitor, "getInstance", mContext);
-		        boolean isAlternateUnlockEnabled = (Boolean) XposedHelpers.callMethod(keyguardUpdateMonitorInstance, "isAlternateUnlockEnabled");
-		        
-		        if (getStaticBooleanField(KeyguardHostView, "DEBUG"))
-		        	Log.v("KeyguardHostView", "showPrimarySecurityScreen(turningOff=" + turningOff + ")");
-		        if (turningOff || isAlternateUnlockEnabled) {
-		            // If we're not turning off, then allow biometric alternate.
-		            // We'll reload it when the device comes back on.
-		            securityMode = XposedHelpers.callMethod(mSecurityModel, "getAlternateFor", securityMode);
-		            securityMode = getStaticObjectField(securityModeEnum, "None"); 
-		        }
-		        XposedHelpers.callMethod(param.thisObject, "showSecurityScreen", securityMode);
-		        param.setResult(false);
+				Object securityMode = XposedHelpers.callMethod(mSecurityModel, "getSecurityMode");
+				Object mContext = getObjectField(param.thisObject, "mContext");
+
+				Object keyguardUpdateMonitorInstance = XposedHelpers.callStaticMethod(keyguardUpdateMonitor, "getInstance", mContext);
+				boolean isAlternateUnlockEnabled = (Boolean) XposedHelpers.callMethod(keyguardUpdateMonitorInstance, "isAlternateUnlockEnabled");
+
+				if (getStaticBooleanField(KeyguardHostView, "DEBUG"))
+					Log.v("KeyguardHostView", "showPrimarySecurityScreen(turningOff=" + turningOff + ")");
+				if (turningOff || isAlternateUnlockEnabled) {
+					// If we're not turning off, then allow biometric alternate.
+					// We'll reload it when the device comes back on.
+					securityMode = XposedHelpers.callMethod(mSecurityModel, "getAlternateFor", securityMode);
+					securityMode = getStaticObjectField(securityModeEnum, "None"); 
+				}
+				XposedHelpers.callMethod(param.thisObject, "showSecurityScreen", securityMode);
+				param.setResult(false);
 			}
 		});
-		
+
 		XposedHelpers.findAndHookMethod(keyguardEffectViewClass, "show", new XC_MethodHook() {
 			@Override
 			protected void beforeHookedMethod(MethodHookParam param) {
@@ -85,7 +85,7 @@ public class UnlockEffect implements IXposedHookLoadPackage {
 				} catch (SettingNotFoundException e) {
 					effect = 2;
 				}
-				
+
 				// 0 = circle, 1 = ripple, 2 = light
 				if (effect == 0) {
 					XposedHelpers.callMethod(param.thisObject, "makeForeground", getStaticObjectField(foregroundEnum, "circle"));
@@ -103,31 +103,31 @@ public class UnlockEffect implements IXposedHookLoadPackage {
 					XposedHelpers.callMethod(mForegroundView, "show");
 					XposedHelpers.callMethod(param.thisObject, "makeBackground", getStaticObjectField(backgroundEnum, "wallpaper"));
 				}
-				
+
 				param.setResult(false);
 			}
 		});
 	}
-/*
+	/*
 	private void hookSettingsPackage(LoadPackageParam lpparam) throws NoSuchMethodException {	
 		final Class<?> lockscreenMenuSettings = findClass("com.android.settings.LockscreenMenuSettings", lpparam.classLoader);
 		final Method createPreferenceHierarchy = XposedHelpers.findMethodBestMatch(lockscreenMenuSettings, "createPreferenceHierarchy");
 
 		XposedHelpers.findAndHookMethod(lockscreenMenuSettings, "createPreferenceHierarchy", new XC_MethodReplacement() {
-			
+
 			@Override
 			protected Object replaceHookedMethod(MethodHookParam param)
 					throws Throwable {
-				
+
 				PreferenceScreen preferenceScreen = (PreferenceScreen) XposedBridge.invokeOriginalMethod(createPreferenceHierarchy,
 						param.thisObject, null);
 				Object lockPatternUtils = getObjectField(param.thisObject, "mLockPatternUtils");
 				boolean isSecure = (Boolean) XposedHelpers.callMethod(lockPatternUtils, "isSecure");
-				
+
 				// Avoid duplicate crap
 				if (!isSecure)
 					return preferenceScreen;
-				
+
 				Method getActivity = null;
 				Activity activity = null;
 				try {
@@ -137,28 +137,28 @@ public class UnlockEffect implements IXposedHookLoadPackage {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 				if (activity == null)
 					return preferenceScreen;
-				
+
 				CheckBoxPreference mMultipleLockScreen = new CheckBoxPreference(activity);
 		        mMultipleLockScreen.setKey("multiple_lock_screen");
 		        mMultipleLockScreen.setTitle(2131300320);
 		        mMultipleLockScreen.setSummary(2131300321);
 		        XposedHelpers.callMethod(mMultipleLockScreen, "setOnPreferenceChangeListener", param.thisObject);
 		        mMultipleLockScreen.setOrder(1);
-		        
+
 		        XposedHelpers.setObjectField(param.thisObject, "mMultipleLockScreen", mMultipleLockScreen);
-				
+
 		        preferenceScreen.addPreference(mMultipleLockScreen);
 		        preferenceScreen.removeAll();
-		        
+
 		        return preferenceScreen;
 			}
 		});
 	}
-	*/
-	
-	
+	 */
+
+
 
 }
